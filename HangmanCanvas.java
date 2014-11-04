40edc4dfb0608cf84b0a82a5644ee407d69560a5
S <s>
2014-11-03 23:40:42 -0800

    {"type":"TI", "runs":"2"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -27,7 +27,7 @@ public class HangmanCanvas extends GCanvas {
  * been guessed so far; unguessed letters are indicated by hyphens.
  */
 	public void displayWord(String word) {
-		/* You fill this in */
+		add( GLabel())
 	}
 
 /**
