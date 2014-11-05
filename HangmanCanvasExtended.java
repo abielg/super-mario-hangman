4bdc78dc538a321bfd9a8708f9663a2c8d24c358
S <s>
2014-11-05 01:39:09 -0800

    {"type":"TI", "runs":"3"}

--- HangmanCanvasExtended.java
+++ HangmanCanvasExtended.java
@@ -4,6 +4,8 @@
  * This file keeps track of the Hangman display.
  */
 
+import java.awt.Color;
+
 import acm.graphics.*;
 
 public class HangmanCanvasExtended extends GCanvas {
@@ -12,26 +14,19 @@ public class HangmanCanvasExtended extends GCanvas {
 	/** Resets the display so that only the scaffold appears */
 	public void reset() {
 		this.removeAll();
-		double scaffoldX = (getWidth() / 2) - BEAM_LENGTH;
-		scaffoldY = ((getHeight() - SCAFFOLD_HEIGHT) / 2) - SCAFFOLD_VERT_OFFSET;
-		
-		//scaffold
-		add(new GLine(scaffoldX, scaffoldY, scaffoldX, scaffoldY + SCAFFOLD_HEIGHT));
-		//beam
-		add(new GLine(scaffoldX, scaffoldY, (scaffoldX + BEAM_LENGTH), scaffoldY));
-		//rope
-		add(new GLine((scaffoldX + BEAM_LENGTH), scaffoldY,(scaffoldX + BEAM_LENGTH), scaffoldY + ROPE_LENGTH));
 		
 		//The word that needs to be guessed will be added to the canvas from the very beginning,
 		//and it will be updated with each method call to "displayWord".
 		wordDisplayed = new GLabel("");
-		wordDisplayed.setFont("Helvetica-24");
-		add(wordDisplayed, LABEL_X_OFFSET, getHeight() - WORD_Y_OFFSET);
+		wordDisplayed.setFont("Helvetica-bold-24");
+		wordDisplayed.setColor(Color.WHITE);
+		add(wordDisplayed, WORD_X_OFFSET, getHeight() - WORD_Y_OFFSET);
 		
 		//Same with the label of the incorrect characters.
 		incorrectChars = new GLabel("");
 		incorrectChars.setFont("Helvetica-15");
-		add(incorrectChars, LABEL_X_OFFSET, getHeight() - INCORRECT_CHARS_Y_OFFSET);
+		incorrectChars.setColor(Color.WHITE);
+		add(incorrectChars, INCORRECT_CHARS_X_OFFSET, getHeight() - INCORRECT_CHARS_Y_OFFSET);
 		
 		//Add Super Mario background
 		GImage background = new GImage("hangmanExtensionbckg.png");
@@ -126,24 +121,15 @@ public class HangmanCanvasExtended extends GCanvas {
 	}
 
 /* Constants for the simple version of the picture (in pixels) */
-	private static final int SCAFFOLD_HEIGHT = 360;
-	private static final int BEAM_LENGTH = 144;
-	private static final int ROPE_LENGTH = 18;
-	private static final int HEAD_RADIUS = 36;
-	private static final int BODY_LENGTH = 144;
-	private static final int ARM_OFFSET_FROM_HEAD = 28;
-	private static final int UPPER_ARM_LENGTH = 72;
-	private static final int LOWER_ARM_LENGTH = 44;
-	private static final int HIP_WIDTH = 36;
-	private static final int LEG_LENGTH = 108;
-	private static final int FOOT_LENGTH = 28;
-	private static final int SCAFFOLD_VERT_OFFSET = 40;
 	
 	/** Distance from left edge of canvas to the word and incorrect character labels */
-	private static final int LABEL_X_OFFSET = 25;
+	private static final int WORD_X_OFFSET = 25;
 	
 	/** Distance from bottom of canvas to the y-coordinate of the word label */
-	private static final int WORD_Y_OFFSET = 50;
+	private static final int WORD_Y_OFFSET = 15;
+	
+	/**Distance from the left edge of canvas to the x-coordinate of the incorrect character label */
+	private static final int INCORRECT_CHARS_X_OFFSET = 170;
 	
 	/**Distance from the bottom of canvas to the y-coordinate of the incorrect character label */
 	private static final int INCORRECT_CHARS_Y_OFFSET = 20;
