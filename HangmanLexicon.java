7a787b27e5f2449598c82baa69285fa76475554c
S <s>
2014-11-04 22:50:49 -0800

    {"type":"TI", "runs":"0"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -7,27 +7,42 @@
 
 import acm.util.*;
 
+import java.io.*;
+import java.util.*;
+
 public class HangmanLexicon {
+	
+	public HangmanLexicon() {
+		BufferedReader rd = null;
+		try {
+			rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+			wordList = new ArrayList<String>();
+			while (true) {
+				String line = rd.readLine();
+				if (line == null)
+					break;
+				wordList.add(line);
+			}
+			rd.close();
+		} catch (IOException ex) {
+				throw new ErrorException(ex);
+			}
+	}
 
 /** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return wordList.size();
 	}
 
 /** Returns the word at the specified index. */
-	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
-	};
+	public String getWord() {
+		int index = rgen.nextInt(0,getWordCount());
+		return wordList.get(index);
+	}
+	
+	/**ArrayList with list of words */
+	private ArrayList<String> wordList;
+	
+    /** Random number generator instance variable */
+    RandomGenerator rgen =  RandomGenerator.getInstance();
 }
