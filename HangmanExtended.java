0b5871f5374b46be68f79a94b67c8889871a3fd4
S <s>
2014-11-05 02:07:49 -0800

    {"type":"TI", "runs":"0"}

--- HangmanExtended.java
+++ HangmanExtended.java
@@ -101,7 +101,6 @@ public class HangmanExtended extends ConsoleProgram {
     		println("You're completely hung.");
     		println("The word was: " + secretWord);
     		println("You lose.");
-    		canvas.moveMario();
     	}
     }
     
